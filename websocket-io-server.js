f726ddf82968983e927ae3f0186fea3c4410a6b5
William Hepton <william.hepton@puzzel.com>
2020-12-14 13:29:38 +0000

    Cors fix

--- websocket-io-server.js
+++ websocket-io-server.js
@@ -1,10 +1,17 @@
 var app = require('express')();
+const cors = require('cors');
+app.use(cors());
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
 
 io.on('connection', function(socket) {
     console.log("A user connected");
-
     socket.emit('test event', 'here is some data');
 });
 
